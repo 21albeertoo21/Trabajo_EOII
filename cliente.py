9cad864a0969af03c04fafed749240449cc777ac
Alberto <aandgom@etsinf.upv.es>
2024-10-27 19:12:09 +0100

    cliente done

--- cliente.py
+++ cliente.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import socket
+import datetime
 
 def mostrar_error_entero():
     ventana_no_entero = tk.Toplevel(ventana)
@@ -16,18 +18,56 @@ def mostrar_error_entero():
     tk.Label(ventana_no_entero, text="Por favor introduce un valor entero").grid(row=0, column=0, padx=20, pady=20)
     ventana_no_entero.mainloop()
 
+def mostrar_fin_conexion():
+    ventana_fin_conexion = tk.Toplevel(ventana)
+    ventana_fin_conexion.title("Conexión finalizada")
+    ventana_fin_conexion.geometry("300x100")  
+
+    # Centrar la ventana de error respecto a la ventana principal
+    ventana_fin_conexion.transient(ventana)
+    ventana_fin_conexion.grab_set()
+    ventana_fin_conexion.update_idletasks()
+    x = ventana.winfo_x() + (ventana.winfo_width() // 2) - (ventana_fin_conexion.winfo_width() // 2)
+    y = ventana.winfo_y() + (ventana.winfo_height() // 2) - (ventana_fin_conexion.winfo_height() // 2)
+    ventana_fin_conexion.geometry(f"+{x}+{y}")
+
+    tk.Label(ventana_fin_conexion, text="Conexión finalizada").grid(row=0, column=0, padx=20, pady=20)
+    ventana_fin_conexion.mainloop()
+
 def boton_click():
     texto = cuadro_texto_mensaje.get()
     texto_IP = cuadro_texto_IP.get()
     texto_puerto = cuadro_texto_puerto.get()
     try:
-        texto_IP_numero = int(texto_IP)
         texto_puerto_numero = int(texto_puerto)
-        cuadro_texto_destino.insert(tk.END, texto+'\n')
+        hora_actual = datetime.datetime.now().strftime("%H:%M:%S")
+        cuadro_texto_destino.insert(tk.END, f"mensaje({texto}) enviado por el cliente a las {hora_actual}"+'\n')
         #Ajustar posición scrollbar para mostrar siempre el último texto
         cuadro_texto_destino.yview_moveto(1.0)
     except:
         mostrar_error_entero()
+    
+    conexion = crear_cliente(texto_puerto, texto_IP)
+    enviar_mensaje(texto, conexion)
+    #cerrar el cliente y la ventana
+    if texto == "FIN":
+        conexion.close()
+        mostrar_fin_conexion()
+        ventana.destroy()
+
+def crear_cliente(texto_puerto, texto_IP):
+    #conexion con el servidor
+    conexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    puerto = int(texto_puerto)
+    direccion_IP = texto_IP
+    conexion.connect(direccion_IP, puerto)
+    #Establecer timeout para la recepción de datos
+    conexion.settimeout(5)
+
+    return conexion
+
+def enviar_mensaje(texto, conexion):
+    conexion.send(texto.encode())
 
 if __name__ == '__main__':
     # Creamos la ventana
