3b5a5faab5b96d94a7c03ead245791b77fc45ab6
oganigl <osganig@gmail.com>
2024-10-27 19:35:35 +0100

    add basic function for the client

--- servidor.py
+++ servidor.py
@@ -1,5 +1,9 @@
 import tkinter as tk
-
+from socket import *
+import pickle 
+import sys
+from datetime import datetime
+import threading
 def mostrar_error_entero():
     ventana_no_entero = tk.Toplevel(ventana)
     ventana_no_entero.title("Error")
@@ -15,16 +19,75 @@ def mostrar_error_entero():
 
     tk.Label(ventana_no_entero, text="Por favor introduce un valor entero").grid(row=0, column=0, padx=20, pady=20)
     ventana_no_entero.mainloop()
+def mostrar_info_puerto():
+    ventana_info = tk.Toplevel(ventana)
+    ventana_info.title("Servidor en ejecución")
+    ventana_info.geometry("300x100")
+    ventana_info.transient(ventana)
+    ventana_info.grab_set()
+    ventana_info.update_idletasks()
+    x = ventana.winfo_x() + (ventana.winfo_width() // 2) - (ventana_info.winfo_width() // 2)
+    y = ventana.winfo_y() + (ventana.winfo_height() // 2) - (ventana_info.winfo_height() // 2)
+    ventana_info.geometry(f"+{x}+{y}")
+
+    # Mensaje de información
+    tk.Label(ventana_info, text="Servidor ya está en ejecución en un puerto.").grid(row=0, column=0, padx=20, pady=20)
+    tk.Label(ventana_info, text="Para cambiar el puerto, cierra el servidor.").grid(row=1, column=0, padx=20, pady=20)
+    
+    
+def handle_client(connection, client_address):
+    print(f"conexion establecida con puerto {client_address[0]}")
+    try:
+        while True:
+            data = connection.recv(1024)
+            data_desarialized = data.decode()
+            if not data:
+                print("no datos del cliente con puerto: ", client_address[1])
+                break
+            ventana.after(0, cuadro_texto_destino.insert, tk.END, f"Cliente en puerto {client_address[1]}: {data_desarialized}\n")
+            ventana.after(0, cuadro_texto_destino.yview_moveto, 1.0)
+    finally:
+        connection.close()
+        print(f"Conexion cerrada con {client_address[1]}")
+        ventana.after(0, cuadro_texto_destino.insert, tk.END, f"Conexion cerrada con {client_address[1]}\n")
+        ventana.after(0, cuadro_texto_destino.yview_moveto, 1.0)
+
+
+
+
+#Create server socket TCP
+def create_server(port):
+    server = socket(AF_INET,SOCK_STREAM)
+    server_address = ('127.0.0.1',port)
+    server.bind(server_address)
+    server.listen(1)
+    print("Servidor escuchando en el puerto",port)
+    ventana.after(0, mostrar_info_puerto)
+    while True:
+        connection,client_address = server.accept()
+        client_thread=threading.Thread(target=handle_client,args=(connection,client_address))
+        client_thread.start()
+        
+        
 
 # Función que se ejecutará al pulsar el botón
 def boton_click():
     texto = cuadro_texto_puerto.get()
     try:
         numero = int(texto)
-        cuadro_texto_destino.insert(tk.END, f"{numero}\n")
-        cuadro_texto_destino.yview_moveto(1.0)
+        #cuadro_texto_destino.insert(tk.END, f"{numero}\n")
+        #cuadro_texto_destino.yview_moveto(1.0)
+        # desactivar cuadro de texto y boton despues de iniciar el servidor
+        cuadro_texto_puerto.config(state='disabled')
+        boton.config(state='disabled')
+        #crear hilo del servidor
+        server_thread = threading.Thread(target=create_server,args=(numero,))  
+        server_thread.start() 
     except ValueError:
         mostrar_error_entero()
+
+
+
 
 if __name__ == '__main__':
     # Creamos la ventana
@@ -48,3 +111,8 @@ if __name__ == '__main__':
     scrollbar.config(command=cuadro_texto_destino.yview)
     # Visualizamos la ventana
     ventana.mainloop()
+    
+
+    
+    
+    
